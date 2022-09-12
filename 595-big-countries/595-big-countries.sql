4300f61a9874f07947772d2854ef805b69b0e034
Kelly H <99680046+CaillouH@users.noreply.github.com>
2022-09-12 13:10:25 -0400

    Time: 554 ms (7.68%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -4,13 +4,5 @@ SELECT
 FROM
     World
 WHERE
-    area >= 3000000 
-
-UNION
-
-SELECT
-    name, population, area
-FROM
-    World
-WHERE
-   population >= 25000000;
+    area >= 3000000
+    OR population >= 25000000;
